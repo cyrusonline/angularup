af9cf48590e86a1cca0c9b85710eb9d8cedbe7c6
C.K. Chan <chanchikin@ied.edu.hk>
2016-01-13 17:05:30 +0800

    init

---
+++ js/controller.js
@@ -0,0 +1,13 @@
+var artistControllers = angular.module('artistControllers',[]);
+
+artistControllers.controller('ListController',function MyController($scope){
+	  $scope.author = {
+			  'name':'Ct',
+	  			'title':'Senior',
+	  			'company':'HKIT'
+	  			
+	  }
+  })
+
+
+  
